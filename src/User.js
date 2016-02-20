d3806cfc27f4cab4d5018119b9c8bdd6f3bfd39a
Jackson Westeen <jcw21@email.arizona.edu>
2016-02-20 12:45:58 -0500

    firebase sync 1st pass

--- src/User.js
+++ src/User.js
@@ -12,27 +12,11 @@ export default class User extends FirebaseConnection {
     _makeUser(usersRef, username) {
         this.username = username;
         this.firebaseUserRef = usersRef.child(username);
-        this.firebaseUserRef.set({
-            status: constants.PLAYER_STATES.ALIVE,
-            location: {
-                x: 0,
-                y: 0,
-                z: 0,
-            }
-        })
     }
 
-    // reducer will call this after reciveing UPDATE_LOCATION action
-    set location(location) {
-        this.firebaseUserRef.update({
-            location
-        });
-    }
-
-    set status(status) {
-        this.firebaseUserRef.update({
-            status
-        })
+    // reducer will call this after reciveing UPDATE_SELF action
+    updateSelf(properties) {
+        this.firebaseUserRef.update(properties);
     }
 }
 
--- src/constants.js
+++ src/constants.js
@@ -1,7 +1,9 @@
 export default {
-	FIREBASE_URI: 'https://ss16-nascent-thunder.firebaseio.com',
-	ACTIONS: {
-		UPDATE_SELF: 'UPDATE_SELF',
-		UPDATE_OTHER: 'UPDATE_OTHER'
-	}
+	FIREBASE_URI: 'https://ss16-nascent-thunder.firebaseio.com'
+}
+
+export const ACTIONS = {
+	INIT_SELF: 'INIT_SELF',
+	UPDATE_SELF: 'UPDATE_SELF',
+	UPDATE_OTHER: 'UPDATE_OTHER'
 }
